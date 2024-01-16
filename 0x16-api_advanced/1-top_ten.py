cf9d6534bfcc3b59058148a86abcc41287dd5772
Emmanuel <oladepogboyega@gmail.com>
2024-01-16 04:28:24 -0800

    My Advanced API task

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16.api.advanced.project by /u/mannyola"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/mannyola)"
     }
     params = {
         "limit": 10
